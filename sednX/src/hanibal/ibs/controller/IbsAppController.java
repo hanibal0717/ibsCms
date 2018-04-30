03397ba9ec22435233fbb7b7f953ca09c0e23bfc
roykhan <ark_@naver.com>
2018-04-30 11:22:42 +0900

    강선구 api 로그인

--- sednX/src/hanibal/ibs/controller/IbsAppController.java
+++ sednX/src/hanibal/ibs/controller/IbsAppController.java
@@ -1,11 +1,25 @@
 package hanibal.ibs.controller;
 
-import org.apache.log4j.Logger;
-import org.springframework.web.bind.annotation.RequestMapping;
-import hanibal.ibs.dao.IbsAppDAO;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import hanibal.ibs.dao.IbsAppDAO;
+import hanibal.ibs.model.webapi.MemberDTO;
+
+@Controller
 public class IbsAppController {
 	Logger log = Logger.getLogger(this.getClass());
+	ObjectMapper mapper = new ObjectMapper();
 	IbsAppDAO ibsAppDAO;
 	
 	public Logger getLog() {
@@ -23,9 +37,53 @@ public class IbsAppController {
 	public void setIbsAppDAO(IbsAppDAO ibsAppDAO) {
 		this.ibsAppDAO = ibsAppDAO;
 	}
-
-	@RequestMapping("/api/app")
-	public String appMain() {
-		return "/ibsAppViews/main.ibs";
+	
+	@RequestMapping("/api/app/Login")
+	public void appLogin(@ModelAttribute MemberDTO dto, HttpServletResponse res) {
+		String account = dto.getAuth_id();
+		String password = dto.getAuth_pass();
+		
+		Map<String, Object> returnJsonMap = new HashMap<String, Object>();
+		Map<String, Object> retMap = new HashMap<String, Object>();
+		
+		int checkFlag = ibsAppDAO.checkMemberInfo(account, password);
+		if(checkFlag == 1) {
+			String token = ibsAppDAO.getMemberToken(account);
+			log.info(token);
+			retMap.put("auth_token", token);
+			
+			returnJsonMap.put("code", "200");
+			returnJsonMap.put("msg", "");
+			returnJsonMap.put("type", "0");
+			returnJsonMap.put("ret", retMap);
+			
+			try {
+				res.setCharacterEncoding("euckr");
+		        res.getWriter().print(mapper.writeValueAsString(returnJsonMap));
+		    } catch (IOException e) {
+		        e.printStackTrace();
+		    }   
+		}
+		else {
+			retMap.put("auth_token", "");
+			
+			returnJsonMap.put("code", "400");
+			returnJsonMap.put("msg", "");
+			returnJsonMap.put("type", "0");
+			returnJsonMap.put("ret", retMap);
+			
+			try {
+				res.setCharacterEncoding("euckr");
+		        res.getWriter().print(mapper.writeValueAsString(returnJsonMap));
+		    } catch (IOException e) {
+		        e.printStackTrace();
+		    }   
+		}
+	}
+	
+	@RequestMapping("/api/app/Vod/BannerList")
+	public void appVodBannerList(@RequestParam String token, HttpServletResponse res) {
+		Map<String, Object> returnJsonMap = new HashMap<String, Object>();
+		Map<String, Object> retMap = new HashMap<String, Object>();
 	}
 }
--- sednX/src/hanibal/ibs/dao/IbsAppDAO.java
+++ sednX/src/hanibal/ibs/dao/IbsAppDAO.java
@@ -1,5 +1,8 @@
 package hanibal.ibs.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.log4j.Logger;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -21,5 +24,16 @@ public class IbsAppDAO {
 	SqlMapClientTemplate sqlMapTemplate;
 	public void setSqlMapTemplate(SqlMapClientTemplate sqlMapTemplate) {
 		this.sqlMapTemplate = sqlMapTemplate;
+	}
+	public int checkMemberInfo(String account, String password) {
+		Map<String, Object> memberInfoMap = new HashMap<String, Object>();
+		memberInfoMap.put("account", account);
+		memberInfoMap.put("password", password);
+		int returnFlag = sqlTemplate.selectOne("checkMemberInfo", memberInfoMap);
+		return returnFlag;
+	}
+	public String getMemberToken(String account) {
+		String returnText = sqlTemplate.selectOne("getMemberToken", account);
+		return returnText;
 	}
 }
